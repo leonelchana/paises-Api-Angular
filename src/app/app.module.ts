8c4dec67e8672de8109880f9076fc1a9420c17b8
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-01-10 18:45:23 -0400

    parte2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
-
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,PaisModule
+    HttpClientModule,
+    SharedModule,
+    PaisModule
   ],
   
   providers: [AppRoutingModule],
