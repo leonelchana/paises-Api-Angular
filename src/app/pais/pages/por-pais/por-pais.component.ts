6da53a93c6c1c1b144edeb00c4c92670e76549bb
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-01-10 23:51:08 -0400

    parte3

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent {
-  termino:string='';
-  hayError:boolean=false;
-  constructor(private paisService:PaisService){}
+  termino: string = '';
+  hayError: boolean = false;
+  paises:Country[]=[];
+  constructor(private paisService: PaisService) {}
 
-  buscar(){
-    this.hayError=false;
-    console.log(this.termino)
-    this.paisService.buscarPais(this.termino)
-    .subscribe(resp=>{
-      console.log(resp);
-    },(err)=>{
-      console.log('Error');
-      console.info(err);
-    });
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises=paises;
+      },
+      (err) => {
+        console.log('ERRor');
+        
+        this.hayError = true;
+        this.paises=[];
+      }
+    );
   }
 }
